70b6cdf34a203217822d3c94757b3f0755f2190e
zackyrr7 <zackyrr7@gmail.com>
2024-03-20 01:45:34 +0700

    auth

---
+++ android/app/src/main/kotlin/com/hz/iamdictive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hz.iamdictive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screen/loginscreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:iamdictive/controller/login_controller.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+  final controller = Get.put(LoginController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Obx(() {
+        if (controller.googleAccount.value == null) {
+          return buildLoginButton();
+        } else {
+          return buildProfileView();
+        }
+      })),
+    );
+  }
+
+  Column buildProfileView() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CircleAvatar(
+          backgroundImage:
+              Image.network(controller.googleAccount.value?.photoUrl ?? '')
+                  .image,
+          radius: 100,
+        ),
+        Text(
+          controller.googleAccount.value?.displayName ?? '',
+          style: Get.textTheme.headlineMedium,
+        ),
+        Text(
+          controller.googleAccount.value?.email ?? '',
+          style: Get.textTheme.bodyMedium,
+        ),
+        ActionChip(
+          label: const Text('Logout'),
+          avatar: const Icon(Icons.logout),
+          onPressed: () {
+            controller.logout();
+          },
+        )
+      ],
+    );
+  }
+
+  FloatingActionButton buildLoginButton() {
+    return FloatingActionButton.extended(
+      label: const Text('Sign in With Google'),
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      onPressed: () {
+        controller.login();
+      },
+    );
+  }
+}
